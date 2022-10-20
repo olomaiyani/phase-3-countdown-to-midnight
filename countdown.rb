05863421f5275e16eb9dff837a543ff80a5485c7
Nana <nanaolomaiyani@gmail.com>
2022-10-20 11:50:37 +0300

    commit tree 8aa4499e454866dd4fe9cf568d832df81cd85459

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown int
+    x =0
+while x < int
+    puts "#{int -x} SECOND(S)!"
+    x +=1
+end 
+"HAPPY NEW YEAR!"
+end
+def countdown_with_sleep seconds_to_midnight
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECONDS(S)!"
+        sleep(5)
+        seconds_to_midnight -=1
+    end
+    "HAPPY NEW YEAR!"
+   
 
-def countdown
 end
